9a89b02abb3d01e42a8d22ab0ce603b384b4ea63
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2023-05-25 00:59:58 +0200

    comment weg

--- src/Components/navigation/navTest.jsx
+++ src/Components/navigation/navTest.jsx
@@ -35,7 +35,6 @@ export default function NavTest() {
           display: showMenu ? "block" : "none",
         }}
       >
-        {/* Temp button */}
         <svg
           onClick={() => {
             setShowMenu(false);
